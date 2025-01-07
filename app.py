fd54063b3994a2c34788de8046a1f4a8eaa2b069
Murakami Aota <e225737@ie.u-ryukyu.ac.jp>
2025-01-07 13:10:05 +0900

    add html

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from flask import Flask, request, jsonify
+from flask import Flask, render_template, send_from_directory
 import socket
 
 def scan_ports(ip,start_port,end_port):
@@ -16,3 +18,26 @@ if __name__ == "__main__":
     start_port = 1
     end_port = 100
     print(f"Open ports on {ip} : {scan_ports(ip, start_port, end_port)}")
+
+app = Flask(__name__)
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+# index.htmlを表示するルート
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/scan', methods=['POST','GET'])
+def scan():
+    data = request.json
+    ip = data['ip']
+    start_port = data['start_port']
+    end_port = data['end_port']
+    open_ports = scan_ports(ip,start_port,end_port)
+    return jsonify({"open_ports": open_ports})
+
+if __name__ == "__main__":
+    app.run(debug=True)
